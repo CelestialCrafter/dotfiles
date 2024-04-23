c14de1b9d0533ac74adc02b7c9cc7313e2b0a265
CelestialCrafter <celestialcraftermc@gmail.com>
2024-04-22 20:09:30 -0500

    fix reload script + small script changes

--- scripts/reload-themes.sh
+++ scripts/reload-themes.sh
@@ -2,15 +2,17 @@
 
 ~/.config/kitty/reload-theme
 
-if command -v nix-shell &> /dev/null then
+pkill -SIGUSR2 waybar
+
+~/.config/scripts/disable-gpu.sh
+
+~/.config/spicetify/reload-theme
+spicetify-cli apply
+
+if [ -x "$(command -v nix-shell)" ]
+then
 	nix-shell ~/.config/neofetch/shell.nix --command ~/.config/neofetch/reload-theme.sh
 else
 	~/.config/neofetch/reload-theme.sh
 fi
-~/.config/spicetify/reload-theme
-spicetify-cli apply
-
-pkill -SIGUSR2 waybar
-
-~/.config/scripts/disable-gpu.sh
 
