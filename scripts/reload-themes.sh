e314fdfe0a50620890b922a0604c41b5caab3d9d
CelestialCrafter <celestialcraftermc@gmail.com>
2024-04-24 06:16:53 -0500

    remove overcomplicated neofetch pfp script change cursor theme to rose pine small btop config change

--- scripts/reload-themes.sh
+++ scripts/reload-themes.sh
@@ -1,18 +1,13 @@
 wal -i $(swww query | grep -o -E 'image: .+' | tail -c+8)
 
-if [ -x "$(command -v nix-shell)" ]
-then
-	nix-shell ~/.config/scripts/shell.nix
-fi
-
 ~/.config/mako/reload-theme
 ~/.config/kitty/reload-theme
-~/.config/spicetify/reload-theme
-~/.config/neofetch/reload-theme.sh
 
 # for some reason reloading waybar with SIGUSR2 randomly crashes my waybar a bit later
 pkill waybar
 hyprctl dispatch exec waybar
+
+~/.config/spicetify/reload-theme
 
 ~/.config/scripts/disable-gpu.sh
 
