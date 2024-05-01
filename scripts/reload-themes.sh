a9c507c55e929db54f1cf75e1721a65300de9107
CelestialCrafter <celestialcraftermc@gmail.com>
2024-05-01 16:00:51 -0500

    more cleanup

--- scripts/reload-themes.sh
+++ scripts/reload-themes.sh
@@ -1,8 +1,10 @@
 wal -i $(swww query | grep -o -E 'image: .+' | tail -c+8)
 
 ~/.config/mako/reload-theme.sh
-~/.config/kitty/reload-theme
-~/.config/obsidian/reload-theme
+~/.config/obsidian/reload-theme.sh
+
+VENCORD_THEME_DIR=/home/celestial/.var/app/dev.vencord.Vesktop/config/vesktop/themes
+install $HOME/.cache/wal/discord.theme.css $VENCORD_THEME_DIR/pywal.theme.css
 
 # for some reason reloading waybar with SIGUSR2 randomly crashes my waybar a bit later
 pkill waybar
@@ -11,6 +13,3 @@ hyprctl dispatch exec waybar
 ~/.config/spicetify/reload-theme
 
 ~/.config/scripts/disable-gpu.sh
-
-VENCORD_THEME_DIR=/home/celestial/.var/app/dev.vencord.Vesktop/config/vesktop/themes
-install $HOME/.cache/wal/discord.theme.css $VENCORD_THEME_DIR/pywal.theme.css
