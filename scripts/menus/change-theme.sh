9d9bf7cc661cdde91c8ee9f8aba8842303f9a3ee
CelestialCrafter <celestialcraftermc@gmail.com>
2024-05-08 16:28:12 -0500

    animations :drool:

--- scripts/menus/change-theme.sh
+++ scripts/menus/change-theme.sh
@@ -1,5 +1,5 @@
 WALLPAPER_DIR=~/Pictures/Wallpapers
 WALLPAPER="$WALLPAPER_DIR/$(ls "$WALLPAPER_DIR" -1 | wofi --prompt "Wallpaper" --show dmenu)"
 
-swww img "$WALLPAPER"
+~/.config/scripts/wallpaper.sh "$1"
 ~/.config/scripts/reload-themes.sh
---
+++ scripts/wallpaper.sh
@@ -0,0 +1,5 @@
+if [[ ! -z $1 ]] then
+	swww img "$1"
+else
+	echo "no wallpaper specified"
+fi
