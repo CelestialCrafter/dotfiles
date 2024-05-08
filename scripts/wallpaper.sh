c24725c3caf1e4ef95af11ade0f6d4e194fbf2ed
CelestialCrafter <celestialcraftermc@gmail.com>
2024-05-08 16:53:53 -0500

    swww config

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,5 +1,12 @@
+positions=("top-left" "top-right")
+position=$(shuf -e ${positions[@]} -n1)
+
 if [[ ! -z $1 ]] then
-	swww img "$1"
+	swww img "$1" \
+	--transition-type grow \
+	--transition-pos "$position" \
+	--transition-duration 2 \
+	--transition-fps 60
 else
 	echo "no wallpaper specified"
 fi
