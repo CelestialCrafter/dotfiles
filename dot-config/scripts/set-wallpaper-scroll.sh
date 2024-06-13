2662f2e43e0fd55dd93c274842de1a6f075a3d74
CelestialCrafter <celestialcraftermc@gmail.com>
2024-06-13 18:37:14 -0500

    firefox (unfinished), discord, fnott, cleanup, and other small changes

--- dot-config/scripts/set-wallpaper-scroll.sh
+++ dot-config/scripts/set-wallpaper-scroll.sh
@@ -5,11 +5,5 @@ fi
 
 # PLEASE keep this in sync with the one in generate-wallpaper-scroll
 IMAGE_SPAN_TAGS=4
-
-WBG_PID=$(pgrep wbg)
-wbg ~/.cache/wallpaper-scroll/$(($1 % IMAGE_SPAN_TAGS)).jpg &
-if [[ ! -z $WBG_PID ]] then
-	# prevent flicker due to wbg being spawned in the background
-	sleep 0.075
-	kill $WBG_PID
-fi
+swww img ~/.cache/wallpaper-scroll/$(($1 % IMAGE_SPAN_TAGS)).jpg \
+	--transition-type none
