486c0017d9b95ac03babbf52d19900626a6ae43f
CelestialCrafter <celestialcraftermc@gmail.com>
2024-10-08 20:53:18 -0500

    remove firefox

--- dot-config/firefox/install.sh
+++
@@ -1,3 +0,0 @@
-FIREFOX_PROFILE_DIR=/home/celestial/.var/app/one.ablaze.floorp/cache/floorp/pmc5a1d6.default-release
-mkdir -p $FIREFOX_PROFILE_DIR/chrome
-install ~/.config/firefox/* $FIREFOX_PROFILE_DIR/chrome
