a26bbd923c87005ede64294ecd65a64bac4a3fec
CelestialCrafter <celestialcraftermc@gmail.com>
2024-11-19 07:35:42 -0600

    split out menubar

--- dot-config/awesome/misc.lua
+++ dot-config/awesome/misc.lua
@@ -1,5 +1,4 @@
 local awful = require("awful")
-local menubar = require("menubar")
 local beautiful = require("beautiful")
 local spiral = require("spiral")
 require("awful.autofocus")
@@ -15,9 +14,6 @@ awful.layout.layouts = {
 local editor = "nvim"
 local terminal = "alacritty"
 local tags = { "1", "2", "3", "4", "5", "6", "7", "8", "S" }
-
--- set terminal for apps that need it
-menubar.utils.terminal = terminal
 
 return {
 	editor = editor,
