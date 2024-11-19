2db13c81c99a6ff5fb8755826323097138b0a468
CelestialCrafter <celestialcraftermc@gmail.com>
2024-11-19 15:10:10 -0600

    make popout menu bigger + small cleanup

--- dot-config/awesome/bar.lua
+++ dot-config/awesome/bar.lua
@@ -21,55 +21,48 @@ awful.screen.connect_for_each_screen(function(s)
 	set_wallpaper(s)
 	awful.tag(misc.tags, s, awful.layout.layouts[1])
 
-	s.taglist = {
-		awful.widget.taglist({
-			screen = s,
-			filter = awful.widget.taglist.filter.all,
-			widget_template = {
+	s.taglist = awful.widget.taglist({
+		screen = s,
+		filter = awful.widget.taglist.filter.all,
+		widget_template = {
+			{
 				{
-					{
-						id = 'text_role',
-						widget = wibox.widget.textbox
-					},
-					id = 'text_margin_role',
-					left = beautiful.margin_l - beautiful.margin_s,
-					right = beautiful.margin_l - beautiful.margin_s,
-					widget = wibox.container.margin
+					id = 'text_role',
+					widget = wibox.widget.textbox
 				},
-				id = 'background_role',
-				widget = wibox.container.background
+				id = 'text_margin_role',
+				left = beautiful.margin_l - beautiful.margin_s,
+				right = beautiful.margin_l - beautiful.margin_s,
+				widget = wibox.container.margin
 			},
-			buttons = gears.table.join(
-				awful.button({}, 1, function(t)
-					t:view_only()
-				end),
-				awful.button({}, 3, awful.tag.viewtoggle)
-			),
-		}),
-		margins = beautiful.margin_s,
-		layout = wibox.container.margin
-	}
+			id = 'background_role',
+			widget = wibox.container.background
+		},
+		buttons = gears.table.join(
+			awful.button({}, 1, function(t)
+				t:view_only()
+			end),
+			awful.button({}, 3, awful.tag.viewtoggle)
+		)
+	})
 
 	s.prompt = awful.widget.prompt()
-	s.clock = {
-		wibox.widget.textclock("<big>%I:%M%P</big>"),
-		font = beautiful.font_l,
-		right = beautiful.margin_s,
-		widget = wibox.container.margin
-	}
-
-	local bar_size = beautiful.margin_xl + beautiful.margin_m
+	s.clock = wibox.widget.textclock("<big>%I:%M%P</big>")
 
 	s.popout = wibox({
 		visible = false,
 		ontop = true,
-		height = bar_size,
+		height = (beautiful.margin_xl * 8) + beautiful.margin_m,
 		width = s.geometry.width,
-		bg = '#0000ff'
+		bg = beautiful.base
 	})
 	s.popout:setup({
-		text   = 'Hello world!',
-		widget = wibox.widget.textbox,
+		{
+			text   = 'Hello world!',
+			widget = wibox.widget.textbox,
+		},
+		margins = beautiful.margin_s,
+		layout = wibox.container.margin,
 	})
 
 	s.popout_button = wibox.widget.textbox("popout")
@@ -80,30 +73,32 @@ awful.screen.connect_for_each_screen(function(s)
 			s.bar.y = 0
 		else
 			s.popout.visible = true
-			s.bar.y = bar_size
+			s.bar.y = s.popout.height
 		end
 	    end)
 	))
 
 	s.bar =  awful.wibar({
-		height = bar_size,
+		height = beautiful.margin_xl + beautiful.margin_m,
 		position = "top",
 		screen = s,
-		ontop =true,
+		ontop = true,
+		bg = beautiful.base
 	})
 	s.bar:setup({
-		s.taglist,
-		nil,
 		{
-			s.prompt,
-			s.popout_button,
-			s.clock,
-			layout = wibox.layout.fixed.horizontal,
+			s.taglist,
+			nil,
+			{
+				s.prompt,
+				s.popout_button,
+				s.clock,
+				layout = wibox.layout.fixed.horizontal,
+			},
+			layout = wibox.layout.align.horizontal,
 		},
-		id = "menus",
-		widget = wibox.container.background,
-		bg = beautiful.base,
-		layout = wibox.layout.align.horizontal
+		margins = beautiful.margin_s,
+		layout = wibox.container.margin,
 	})
 end)
 
