916c90b0da4d423261b224082b5d3126e9463ecf
CelestialCrafter <celestialcraftermc@gmail.com>
2024-11-19 08:30:02 -0600

    fix prompt execute

--- dot-config/awesome/keybinds.lua
+++ dot-config/awesome/keybinds.lua
@@ -15,7 +15,7 @@ local globalkeys = gears.table.join(
 	awful.key({ modkey }, "x", function()
 		awful.prompt.run({
 			prompt = "Run Lua code: ",
-			textbox = awful.screen.focused().mypromptbox.widget,
+			textbox = awful.screen.focused().prompt.widget,
 			exe_callback = awful.util.eval,
 			history_path = awful.util.get_cache_dir() .. "/history_eval",
 		})
