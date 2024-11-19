9aa7e88d2a9878ac1b07276d55c31008e4f3780b
CelestialCrafter <celestialcraftermc@gmail.com>
2024-11-19 07:35:42 -0600

    use dpi + titlebar on the side

--- dot-config/awesome/clients.lua
+++ dot-config/awesome/clients.lua
@@ -2,6 +2,8 @@ local awful = require("awful")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
 
+local dpi = beautiful.xresources.apply_dpi
+
 -- prevent unreachable clients if screens change
 client.connect_signal("manage", function(c)
 	if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
@@ -11,16 +13,16 @@ end)
 
 -- titlebar
 client.connect_signal("request::titlebars", function(c)
-	awful.titlebar(c):setup({
-		awful.titlebar.widget.titlewidget(c),
+	awful.titlebar(c, { position = 'left', size = dpi(32) }):setup({
+		awful.titlebar.widget.iconwidget(c),
 		nil,
 		{
 			awful.titlebar.widget.maximizedbutton(c),
 			awful.titlebar.widget.stickybutton(c),
 			awful.titlebar.widget.closebutton(c),
-			layout = wibox.layout.fixed.horizontal(),
+			layout = wibox.layout.fixed.vertical,
 		},
-		layout = wibox.layout.align.horizontal,
+		layout = wibox.layout.align.vertical,
 	})
 end)
 
@@ -32,14 +34,13 @@ end)
 local function warp_to_focused(c)
     if mouse.object_under_pointer() ~= c then
         local geometry = c:geometry()
-        local x = geometry.x + geometry.width/2
-        local y = geometry.y + geometry.height/2
-        mouse.coords({x = x, y = y}, true)
+        local x = geometry.x + geometry.width / 2
+        local y = geometry.y + geometry.height / 2
+        mouse.coords({ x = x, y = y }, true)
     end
 end
 
 client.connect_signal("swapped", warp_to_focused)
-
 client.connect_signal("focus", function(c)
 	c.border_color = beautiful.border_focus
 	warp_to_focused(c)
