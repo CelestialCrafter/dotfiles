62e239842bb3f3500a500c7b1d9d1c141dadb724
CelestialCrafter <celestialcraftermc@gmail.com>
2024-11-19 07:35:42 -0600

    clean up theme file

--- dot-config/awesome/keybinds.lua
+++ dot-config/awesome/keybinds.lua
@@ -1,6 +1,5 @@
 local awful = require("awful")
 local gears = require("gears")
-local naughty = require("naughty")
 local menubar = require("menubar")
 local popup = require("awful.hotkeys_popup")
 local misc = require("misc")
@@ -161,9 +160,6 @@ local clientkeys = gears.table.join(
 )
 
 local clientbuttons = gears.table.join(
-	awful.button({}, 1, function(c)
-		c:emit_signal("request::activate", "mouse_click", { raise = true })
-	end),
 	awful.button({ modkey }, 1, function(c)
 		c:emit_signal("request::activate", "mouse_click", { raise = true })
 		awful.mouse.client.move(c)
