a8dabc798e28e82ae260e7584d532d6ae3106152
CelestialCrafter <celestialcraftermc@gmail.com>
2024-11-20 15:56:26 -0600

    place windows above if theyre sticky

--- dot-config/awesome/client/misc.lua
+++ dot-config/awesome/client/misc.lua
@@ -12,10 +12,20 @@ client.connect_signal("manage", function(c)
 	c.shape = beautiful.rounded_rect
 end)
 
+-- rounded corners if not fullscreen
 client.connect_signal("property::fullscreen", function(c)
 	if c.fullscreen then
 		c.shape = gears.shape.rect
 	else
 		c.shape = beautiful.rounded_rect
+	end
+end)
+
+-- place above if sticky
+client.connect_signal("property::sticky", function(c)
+	if c.sticky then
+		c.above = true
+	else
+		c.above = false
 	end
 end)
