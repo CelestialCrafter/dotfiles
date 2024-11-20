c6d5831c12396fea9ed5dd3a8b470efbe42be374
CelestialCrafter <celestialcraftermc@gmail.com>
2024-11-20 16:11:28 -0600

    clean up shapes

--- dot-config/awesome/screen/bar/init.lua
+++ dot-config/awesome/screen/bar/init.lua
@@ -1,5 +1,6 @@
 local awful = require("awful")
 local wibox = require("wibox")
+local gears = require("gears")
 local beautiful = require("beautiful")
 
 local taglist = require("screen.bar.taglist")
@@ -12,7 +13,10 @@ return function(s)
 		height = beautiful.margin_xl + beautiful.margin_m,
 		position = "top",
 		screen = s,
-		bg = beautiful.base
+		bg = beautiful.base,
+		shape = function(cr, w, h)
+			gears.shape.partially_rounded_rect(cr, w, h, false, false, true, true, beautiful.margin_m)
+		end
 	})
 
 	local popout_button = wibox.widget.textbox("popout")
@@ -32,7 +36,7 @@ return function(s)
 			layout = wibox.layout.align.horizontal,
 		},
 		margins = beautiful.margin_s,
-		layout = wibox.container.margin,
+		layout = wibox.container.margin
 	})
 end
 
