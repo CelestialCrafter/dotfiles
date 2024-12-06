2cb9f122a128ccb2ab1362035eac298b3404beb1
CelestialCrafter <celestialcraftermc@gmail.com>
2024-12-05 21:39:34 -0600

    wip control center

--- dot-config/awesome/popups/init.lua
+++ dot-config/awesome/popups/init.lua
@@ -1,8 +1,10 @@
 local overview = require("popups.overview")
 local launcher = require("popups.launcher")
 local bar = require("popups.bar")
+local control_center = require("popups.control_center")
 
 return function(s)
+	s.control_center = control_center()
 	s.launcher = launcher()
 	s.overview = overview(s)
 	bar(s)
