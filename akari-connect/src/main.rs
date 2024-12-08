10db3b6c0045b548671c985e1aff05125d3852eb
CelestialCrafter <celestialcraftermc@gmail.com>
2024-12-08 06:02:24 -0600

    basic dbus test

--- akari-connect/src/main.rs
+++ akari-connect/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use dbus::blocking::Connection;
+use eyre::Result;
+
+fn main() -> Result<()> {
+    let conn = Connection::new_session()?;
+    let proxy = conn.with_proxy("org.freedesktop.DBus", "/", Duration::from_millis(5000));
+
+    let (names,): (Vec<String>,) = proxy.method_call("org.freedesktop.DBus", "ListNames", ())?;
+
+    for name in names {
+        println!("{}", name);
+    }
+
+    Ok(())
 }
