38bea0fdb418b26b3cc14bbb63bca351f7383272
CelestialCrafter <celestialcraftermc@gmail.com>
2024-12-08 04:41:52 -0600

    Back out "wip control center"
    This backs out commit 2fb55f2ed364092e4341503e76b79a15c628cba9.

--- dot-config/awesome/popups/init.lua
+++ dot-config/awesome/popups/init.lua
@@ -1,10 +1,8 @@
 local overview = require("popups.overview")
 local launcher = require("popups.launcher")
 local bar = require("popups.bar")
-local control_center = require("popups.control_center")
 
 return function(s)
-	s.control_center = control_center()
 	s.launcher = launcher()
 	s.overview = overview(s)
 	bar(s)
