40ff4044c6d7036c61ae890b06b29ba18f1185aa
CelestialCrafter <celestialcraftermc@gmail.com>
2024-12-08 06:17:31 -0600

    communicate with awm

--- akari-connect/src/main.rs
+++ akari-connect/src/main.rs
@@ -3,15 +3,14 @@ use std::time::Duration;
 use dbus::blocking::Connection;
 use eyre::Result;
 
+const AWM_DEST: &str = "org.awesomewm.awful";
+
 fn main() -> Result<()> {
     let conn = Connection::new_session()?;
-    let proxy = conn.with_proxy("org.freedesktop.DBus", "/", Duration::from_millis(5000));
+    let proxy = conn.with_proxy(AWM_DEST, "/", Duration::from_millis(5000));
 
-    let (names,): (Vec<String>,) = proxy.method_call("org.freedesktop.DBus", "ListNames", ())?;
-
-    for name in names {
-        println!("{}", name);
-    }
+    let (result,): (f64,) = proxy.method_call(format!("{}.Remote", AWM_DEST), "Eval", ("return 1+1",))?;
+    println!("{}", result);
 
     Ok(())
 }
