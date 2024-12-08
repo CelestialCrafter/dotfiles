ab2f97aaf484e74e75c76fca9dd54bb97a59eb5a
CelestialCrafter <celestialcraftermc@gmail.com>
2024-12-08 07:16:39 -0600

    custom dbus protocol

--- akari-connect/src/main.rs
+++ akari-connect/src/main.rs
@@ -3,14 +3,13 @@ use std::time::Duration;
 use dbus::blocking::Connection;
 use eyre::Result;
 
-const AWM_DEST: &str = "org.awesomewm.awful";
+const AWM_DEST: &str = "org.awesomewm.akariconnect";
 
 fn main() -> Result<()> {
     let conn = Connection::new_session()?;
     let proxy = conn.with_proxy(AWM_DEST, "/", Duration::from_millis(5000));
 
-    let (result,): (f64,) = proxy.method_call(format!("{}.Remote", AWM_DEST), "Eval", ("return 1+1",))?;
-    println!("{}", result);
+    proxy.method_call(AWM_DEST, "Test", ())?;
 
     Ok(())
 }
--- dot-config/awesome/connect/init.lua
+++ dot-config/awesome/connect/init.lua
@@ -1,2 +1,53 @@
-local gio = require("lgi").Gio
+-- ref: https://github.com/awesomeWM/awesome/blob/master/lib/naughty/dbus.lua
+
+local lgi = require("lgi")
+local gio = lgi.Gio
+local gobject = lgi.GObject
+
+local bus_name = "org.awesomewm.akariconnect"
+
+local methods = {}
+
+function methods.Test(sender, object_path, interface, method, parameters, invocation)
+    require("naughty").notify({
+        text = tostring(sender)
+    })
+
+    invocation:return_value()
+end
+
+local function method_call(_, sender, object_path, interface, method, parameters, invocation)
+    methods[method](sender, object_path, interface, method, parameters, invocation)
+end
+
+local function on_bus_acquire(conn, _)
+    local function arg(name, sig)
+        return gio.DBusArgInfo { name = name, signature = sig }
+    end
+
+    local method = gio.DBusMethodInfo
+    local signal = gio.DBusSignalInfo
+
+    local interface_info = gio.DBusInterfaceInfo {
+        name = bus_name,
+        methods = {
+            method {
+                name = "Test",
+            }
+        }
+    }
+
+    conn:register_object(
+        "/",
+        interface_info,
+        gobject.Closure(method_call)
+    )
+end
+
+gio.bus_own_name(
+    gio.BusType.SESSION,
+    bus_name,
+    gio.BusNameOwnerFlags.NONE,
+    gobject.Closure(on_bus_acquire)
+)
 
