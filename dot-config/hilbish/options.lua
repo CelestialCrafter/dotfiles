d2f1c2a7f277506f767696c393a554c3313e2437
CelestialCrafter <celestialcraftermc@gmail.com>
2024-12-27 01:29:49 -0600

    change $EDITOR

--- dot-config/hilbish/options.lua
+++ dot-config/hilbish/options.lua
@@ -7,6 +7,7 @@ hilbish.opts.motd = false
 hilbish.opts.fuzzy = true
 hilbish.inputMode("vim")
 
-hilbish.alias("vim", "$EDITOR")
+local _, editor, _ = hilbish.run("echo $EDITOR", false)
+hilbish.alias("vim", editor:gsub("%s+$", ""))
 hilbish.alias("top", "btop")
 hilbish.alias("ls", "ls --color=always")
