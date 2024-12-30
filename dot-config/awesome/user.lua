bc97ff5ac0f95eaa5361809dc92bd5659eb9f1ca
CelestialCrafter <celestialcraftermc@gmail.com>
2024-12-30 13:39:33 -0600

    use terminal desktop entry

--- dot-config/awesome/user.lua
+++ dot-config/awesome/user.lua
@@ -1,6 +1,5 @@
 local user = {}
 
-user.terminal = "alacritty"
 user.wallpaper = "~/Pictures/Wallpapers/normal.jpg"
 user.profile = "~/Pictures/user.png"
 user.font = "sans-serif 11"
@@ -8,6 +7,7 @@ user.preview_update_interval = 0.5
 user.connect_update_interval = 0.5
 user.titlebar_position = "left"
 user.bar_position = "top"
+user.terminal = "Alacritty.desktop"
 user.pinned_apps = {
 	Alacritty = "Alacritty.desktop",
 	floorp = "one.ablaze.floorp.desktop",
