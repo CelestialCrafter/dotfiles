714d5f3f39f77f2ba1ecdcac3b16f2b2af7b190d
CelestialCrafter <celestialcraftermc@gmail.com>
2025-04-17 20:34:42 -0500

    remove garbage day

--- dot-config/nvim/lua/plugins/init.lua
+++ dot-config/nvim/lua/plugins/init.lua
@@ -1,10 +1,1 @@
-return {
-	{ "wakatime/vim-wakatime", event = "VeryLazy" },
-	{
-		"zeioth/garbage-day.nvim",
-		dependencies = "neovim/nvim-lspconfig",
-		event = "VeryLazy",
-		-- 10 minutes
-		opts = { grace_period = 10 * 60 },
-	},
-}
+return { "wakatime/vim-wakatime", event = "VeryLazy" }
