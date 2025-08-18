1f432d86fdb9f8032dd5202fedceb4db726c2c39
CelestialCrafter <celestialcraftermc@gmail.com>
2025-08-18 15:55:12 -0500

    add normal color at the end of fish prompt

--- dot-config/fish/functions/fish_prompt.fish
+++ dot-config/fish/functions/fish_prompt.fish
@@ -1,14 +1,13 @@
 function fish_prompt
-	if test $status -eq 0
-		set -f status_color (set_color $fish_color_user)
-	else
-		set -f status_color (set_color $fish_color_status)
-	end
+    if test $status -eq 0
+        set -f status_color (set_color $fish_color_user)
+    else
+        set -f status_color (set_color $fish_color_status)
+    end
 
-	printf "%s%s %s%s%s " \
-		$status_color \
-		$USER \
-		(set_color $fish_color_cwd) \
-		(basename (string replace $HOME "~" $PWD)) \
-		(fish_jj_prompt)
+    printf "%s%s %s%s%s%s " \
+        $status_color $USER \
+        (set_color $fish_color_cwd) (basename (string replace $HOME "~" $PWD)) \
+        (fish_jj_prompt) \
+        (set_color normal)
 end
